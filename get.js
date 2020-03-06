52ea7a1243fab573273221cd39803321f68cbd61
ColtonOsterlund <coltonericosterlund@gmail.com>
2020-03-06 11:42:55 -0700

    query parameters for courses

--- get.js
+++ get.js
@@ -18,28 +18,80 @@ router.get("/courses", jsonParser, (req, res) => {
 
     var schoolID = req.query.schoolID;
     var programID = req.query.programID;
-    console.log(schoolID);
 
-    mysqlHelper.sqlQuery("SELECT * FROM course", null, (err, rows) => {
-        if(err != null){
-            return res.send("Error: " + err)
-        }
-        else{
-            var jsonObjects = []
-            
-            rows.forEach(function(course){
-                var courseObject = {
-                    courseName: course.courseName,
-                    courseID: course.courseID,
-                    level: course.level
-                }
+    if(schoolID == undefined && programID == undefined){ //NO QUERY PARAMETERS
+        mysqlHelper.sqlQuery("SELECT * FROM course", null, (err, rows) => {
+            if(err != null){
+                return res.send("Error: " + err)
+            }
+            else{
+                var jsonObjects = []
+                
+                rows.forEach(function(course){
+                    var courseObject = {
+                        courseName: course.courseName,
+                        courseID: course.courseID,
+                        level: course.level
+                    }
 
-                jsonObjects.push(course);
-            })
+                    jsonObjects.push(course);
+                })
 
-            return res.send(JSON.stringify(jsonObjects))
-        }
-    });
+                return res.send(JSON.stringify(jsonObjects))
+            }
+        });
+    }
+
+    else if(schoolID != undefined && programID == undefined){ //SCHOOL QUERY PARAMETER
+        mysqlHelper.sqlQuery("SELECT * FROM course AS C, schoolrelcourse as SRC WHERE C.courseID = SRC.courseID AND SRC.schoolID = ?", [schoolID], (err, rows) => {
+            if(err != null){
+                return res.send("Error: " + err)
+            }
+            else{
+                var jsonObjects = []
+                
+                rows.forEach(function(course){
+                    var courseObject = {
+                        courseName: course.courseName,
+                        courseID: course.courseID,
+                        level: course.level
+                    }
+
+                    jsonObjects.push(course);
+                })
+
+                return res.send(JSON.stringify(jsonObjects))
+            }
+        });
+    }
+
+    else if(schoolID == undefined && programID != undefined){ //PROGAM QUERY PARAMETER
+        mysqlHelper.sqlQuery("SELECT * FROM course AS C, programrelcourse as PRC WHERE C.courseID = PRC.courseID AND PRC.programID = ?", [programID], (err, rows) => {
+            if(err != null){
+                return res.send("Error: " + err)
+            }
+            else{
+                var jsonObjects = []
+                
+                rows.forEach(function(course){
+                    var courseObject = {
+                        courseName: course.courseName,
+                        courseID: course.courseID,
+                        level: course.level
+                    }
+
+                    jsonObjects.push(course);
+                })
+
+                return res.send(JSON.stringify(jsonObjects))
+            }
+        });
+    }
+
+    else if(schoolID != undefined && programID != undefined){ //SCHOOL & PROGRAM QUERY PARAMETERS
+        //TODO
+    }
+
 })
 
 
