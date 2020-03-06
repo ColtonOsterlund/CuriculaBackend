76d4d9498b1b04779d326dba49c619032a1dff68
ColtonOsterlund <coltonericosterlund@gmail.com>
2020-03-06 16:43:16 -0700

    cors

--- server.js
+++ server.js
@@ -18,18 +18,6 @@ server.use(getRouter)
 server.use(postRouter)
 server.use(cors())
 
-server.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-   });
-
-   server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-  });
-
 
 function mySQLConnectionTest(){
     mysqlHelper.sqlQuery("CREATE TABLE TEST(test VARCHAR(40))", null, (err, rows) => {
