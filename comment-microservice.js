1efc9a22615700ba5b9a34c41e9615190ef88022
CheesusChrist <32204044+CheesusChrist@users.noreply.github.com>
2020-03-11 10:11:17 -0600

    added missing files for microservice

--- comment-microservice.js
+++ comment-microservice.js
@@ -1,3 +1,25 @@
 const express = require('express')
 const mysqlHelper = require('./MySQLHelper.js')
 
+function createCommentEvent(newEvent) {
+    newEvent.timeStamp = Date.now()
+    newEvent.eventID = 123  //TODO generate unique eventID (or let SQL)
+
+    
+    
+
+    if (newEvent.mode == 'create') {    //if create mode, create an event and a new comment ID
+        newEvent.commentID = 432 //TODO generate unique comment IDs
+    } else if (newEvent.mode == 'edit') {   //if edit more, create an event for given comment ID
+        newEvent.commentID = newEvent.parent-id
+    }
+    console.log("creating event with \n" + JSON.stringify(newEvent))
+}
+
+function createVoteEvent(newEvent) {
+    //generate ID
+    //write into event store
+}
+
+module.exports.createCommentEvent = createCommentEvent;
+module.exports.createVoteEvent = createVoteEvent;
