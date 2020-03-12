70c4239039763092d044dd4ea603c19e6fd0a176
Alexandra Glodzinski <alexandra.glodzinski@ucalgary.ca>
2020-03-11 22:35:30 -0600

    endpoint and what not

--- commentMSHelper.js
+++ commentMSHelper.js
@@ -1,16 +1,5 @@
 //commentMShelper.js
 
-const comment = require('../commentMS/comment.js');
-const express = require('express')
-
-const router = express.Router()
-
-var jsonParser = bodyParser.json()
-
-//do whatever we need to do with comment here:
-//Probably any and all functions that use comment
-
-//sample of how to insert comment with query
 function insertCommentToDatabase(parentID, commentID, userID, theText, counter){
 	
 	if (parentID == null){//parent comment
@@ -23,72 +12,16 @@ function insertCommentToDatabase(parentID, commentID, userID, theText, counter){
 	var today = new Date();
 	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 		
-	String query2 = "INSERT INTO comment (commentID, authorUser, datePosted, text, counter) VALUES (" + commentID+ "," + userID + "," + date + "," + theText + "," + counter + ");"; 
+	var query2 = "INSERT INTO comment (commentID, authorUser, datePosted, text, counter) VALUES (" + commentID+ "," + userID + "," + date + "," + theText + "," + counter + ");"; 
 		
 		
 	//Finally apply query 1 and 2 to database after user creates and after passign all necessary values
 }
-	
-//maybe combine child and parent
-//child from parent end
-router.get('/comment', jsonParser, (req, res) => {
-	
-	var commentID = req.body.commentID
-	
-	var object = mysqlHelper.sqlQuery("SELECT * FROM childComment WHERE parentID = ?", [parentID], (err, objects) => { //callback function so that query loads before data is checked/sent back to user
 
-		if(err != null){
-			return console.log("ERROR : " + err)
-		}
 
-		if(objects[0] == undefined){ //childComment did not match - childComment not in database
-			return res.send("Parent Comment ID is Incorrect")
-		}
-		
-		var jsonObjects = []
-                
-        rows.forEach(
-			var commentObject = new comment(childComment.commentID, childComment.comment, childComment.userID, childComment.videoId, childComment.parentCommentID, childComment.timeStamp)
+//create delete comment function deleting the comment from all places in db
 
-			jsonObjects.push(commentObject);
-        )
 
-		return res.send(JSON.stringify(jsonObjects)) //this sends all childComment objects
-	})
-})
-
-//parent from post end
-router.post('/post/comment', jsonParser, (req, res) => {
-	
-	var parentID = req.body.parentID
-	
-	var object = mysqlHelper.sqlQuery("SELECT * FROM parentComment WHERE parentID = ?", [parentID], (err, objects) => { //callback function so that query loads before data is checked/sent back to user
-
-		if(err != null){
-			return console.log("ERROR : " + err)
-		}
-
-		if(objects[0] == undefined){ //childComment did not match - childComment not in database
-			return res.send("Parent Comment ID is Incorrect")
-		}
-		
-		var jsonObjects = []
-                
-        rows.forEach(
-			var commentObject = comment(parentComment.commentID, parentComment.comment, parentComment.userID, parentComment.videoId, parentComment.parentCommentID, parentComment.timeStamp)
-
-			jsonObjects.push(commentObject);
-        )
-
-		return res.send(JSON.stringify(jsonObjects)) //this sends all childComment objects
-
-	})
-	
-})
 	
 	
-module.exports.insertCommentToDatabase = insertCommentToDatabase;
-	
-	
-	
-	
+module.exports.insertCommentToDatabase = insertCommentToDatabase
