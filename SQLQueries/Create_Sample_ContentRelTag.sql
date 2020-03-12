d6fde1d8df283b7cb238305b141d868784425595
Alexandra Glodzinski <alexandra.glodzinski@ucalgary.ca>
2020-03-11 21:48:15 -0600

    Created sample contentRelTags and topic tags

---
+++ SQLQueries/Create_Sample_ContentRelTag.sql
@@ -0,0 +1,17 @@
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("1", "1");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("2", "2");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("3", "3");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("4", "4");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("5", "5");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("6", "6");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("7", "7");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("8", "1");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("9", "2");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("10", "3");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("1", "4");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("2", "5");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("3", "6");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("4", "7");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("5", "1");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("6", "2");
+INSERT INTO contentRelTag(contentID, tagid) VALUES ("7", "3");
