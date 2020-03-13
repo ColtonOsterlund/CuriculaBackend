df030a12c1bc68660c7478fe74e3cf179225c56c
Alexandra Glodzinski <alexandra.glodzinski@ucalgary.ca>
2020-03-12 22:08:22 -0600

    Changed the file to compensate the removal of commentMSHelper

--- commentPost.js
+++ commentPost.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const commentMSHelper = require('./commentMSHelper.js')
 const router = express.Router()
 const authenticationRouter = require('./authorization.js')
+const mysqlHelper = require('../MySQLHelper.js')
 
 var bodyParser = require('body-parser')
 
@@ -16,6 +16,22 @@ router.post('/comment/create', jsonParser, authenticationRouter.authorizeUser, (
     var userID = req.body.userID
     var theText = req.body.theText
 	
-	commentMSHelper.insertCommentToDatabase(parentID, commentID, userID, theText)
+	if (parentID == null){//parent comment
+		var query = "INSERT INTO parentComment(parentID) VALUES (?);";
+		mysqlHelper.sqlQuery(query, [commentID], err_handler)
+	} else{//child comment
+		var query = "INSERT INTO childComment(childID, parentID) VALUES (?, ?);";
+		mysqlHelper.sqlQuery(query, [commentID, parentID], err_handler)
+	}
+	
+
+	//today code obtained from https://tecadmin.net/get-current-date-time-javascript/
+	var today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+	var query2 = "INSERT INTO comments (commentID, authorUser, datePosted, text, counter) VALUES (?, ?, ?, ?, ?);"; 
+	 mysqlHelper.sqlQuery(query2, [commentID, userID, date, theText, counter], err_handler)
+	
+	//commentMSHelper.insertCommentToDatabase(parentID, commentID, userID, theText)
 
  })
