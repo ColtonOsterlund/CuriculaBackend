5818437d7c252dce1a3b55702ef81980d2645cdc
ColtonOsterlund <coltonericosterlund@gmail.com>
2020-04-02 11:20:54 -0600

    post route for videos

--- post.js
+++ post.js
@@ -33,6 +33,26 @@ router.post("/comments", jsonParser, authorization.authorizeUser, (req, res) => 
 
 })
 
+router.post("/video", jsonParser, authorization.authorizeUser, (req, res) => {
+    var videoLink = req.body.videoLink
+	var videoDescription = req.body.videoDescription
+    var relatedCourseID = req.body.relatedCourseID
+    
+    var posterID = req.header("user-id")
+    var videoID = uuid.v4();
+    var datePosted = new Date();
+    
+
+    mysqlHelper.sqlQuery("INSERT INTO content (videoID, videoLink, description, datePosted, relatedCourseID, verifiedFlag, posterID) VALUES (?, ?, ?, ?, ?, ?, ?)", [videoID, videoLink, videoDescription, datePosted, relatedCourseID, "0", posterID], (err, rows) => {
+		if (err != null) {
+			return res.json([{message: err}])
+		}
+		else {
+			return res.json([{message: "Success"}])
+		}
+	})
+})
+
 router.post("/vote", jsonParser, authorization.authorizeUser, (req, res) => {
     em.generateEvent({
         type: "vote",
